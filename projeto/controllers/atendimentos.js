cf8ea43fb180491e702fee17314e769ab8078644
WIllian Paulino <willianpaulinox@gmail.com>
2022-03-29 11:38:39 -0300

    Finalized the project.

--- projeto/controllers/atendimentos.js
+++ projeto/controllers/atendimentos.js
@@ -3,12 +3,29 @@ const Atendimento = require('../models/atendimentos');
 
 module.exports = function(app){
     app.get("/atendimentos", function(req, res){
-        res.send("Você está na rota de atendimentos e está realizand um GET");
+        Atendimento.lista(res)
     });
+
+    app.get("/atendimentos/:id", function(req, res){
+        const id = parseInt(req.params.id)
+
+        Atendimento.buscaPorId(id, res)
+        
+    })
 
     app.post("/atendimentos", function(req, res){
         const atendimento = req.body
-        Atendimento.adiciona(atendimento)
-        res.send("Você está na rota de atendimentos e está realizando um POST");
+        Atendimento.adiciona(atendimento, res)
+    })
+
+    app.patch("/atendimentos/:id", function (req, res)  {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+        Atendimento.altera(id, valores, res)
+    })
+
+    app.delete("/atendimentos/:id", function(req, res){
+        const id = parseInt(req.params.id)
+        Atendimento.deleta(id, res)
     })
 }
